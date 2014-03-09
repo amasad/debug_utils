3f4abbe376eda345c7bfae7fd5f0f29c855a6f11
Amjad Masad <amjad.masad@gmail.com>
2014-03-09 15:20:57 -0400

    dont checkin build. And add reload support to extension

--- script/build.js
+++ script/build.js
@@ -1,9 +1,17 @@
 var fs = require('fs');
 var util = require('util');
 
+var pre = 'function inject() {\n';
+var post = [
+'',
+'}',
+'chrome.devtools.network.onNavigated.addListener(inject);',
+'inject();'
+].join('\n');
+
 var code = fs.readFileSync(__dirname + '/../du.js').toString();
 fs.writeFileSync(
   __dirname + '/../ext/devtools.js',
-  'chrome.devtools.inspectedWindow.eval(JSON.parse(' +
-    util.inspect(JSON.stringify(code)) + '));'
+  pre + '\tchrome.devtools.inspectedWindow.eval(JSON.parse(' +
+    util.inspect(JSON.stringify(code)) + '));' + post
 );
